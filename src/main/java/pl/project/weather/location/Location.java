6d8442a3544bbc123a56229e8adee6af5f8df833
Michal Paukszto <michal.paukszto@gmail.com>
2021-05-29 19:33:43 +0200

    review

--- src/main/java/pl/project/weather/location/Location.java
+++ src/main/java/pl/project/weather/location/Location.java
@@ -15,19 +15,14 @@ public class Location {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
     @Column(name = "city_name")
     private String cityName;
-
     @Column(name = "longitude")
     private Float longitude;
-
     @Column(name = "latitude")
     private Float latitude;
-
     @Column(name = "region")
     private String region;
-
     @Column(name = "country")
     private String country;
 }
--- src/main/java/pl/project/weather/location/LocationValidator.java
+++ src/main/java/pl/project/weather/location/LocationValidator.java
@@ -3,18 +3,19 @@ package pl.project.weather.location;
 public class LocationValidator {
 
     public void validateLocation(String cityName, Float logitude, Float latitude, String country) {
-        if (cityName.isBlank()) {
+        if (cityName.isBlank()) { // todo NullPointerException may occur
             throw new IllegalArgumentException("Nazwa miasta nie może być pusta");
         } else if (logitude > 180 || logitude < -180) {
             throw new IllegalArgumentException("Taka długość geograficzna nie istnieje");
         } else if (latitude > 90 || latitude < -90) {
             throw new IllegalArgumentException("Taka szerokość geograficzna nie istnieje");
-        } else if (country.isBlank()) {
+        } else if (country.isBlank()) { // todo NullPointerException may occur
             throw new IllegalArgumentException("Nazwa kraju nie może być pusta");
         }
     }
-    public String validateRegion(String region){
-        if(region.isBlank())
+
+    public String validateRegion(String region) {
+        if (region.isBlank()) // todo NullPointerException may occur
             return null;
         else return region;
     }
