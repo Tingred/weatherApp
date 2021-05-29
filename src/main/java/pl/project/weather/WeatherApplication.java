1094e4949f37a13ff2fe9834b4d32e845008976c
Tingred <exiles1@wp.pl>
2021-05-29 15:59:24 +0200

    WeatherApplication - main added

--- src/main/java/pl/project/weather/WeatherApplication.java
+++ src/main/java/pl/project/weather/WeatherApplication.java
@@ -1,8 +1,18 @@
 package pl.project.weather;
 
+import pl.project.weather.location.Location;
+import pl.project.weather.location.LocationController;
+import pl.project.weather.location.LocationRepositoryImpl;
+import pl.project.weather.location.LocationService;
+import pl.project.weather.uInterface.UserInterface;
+
 public class WeatherApplication {
 
     public static void main(String[] args) {
-
+        LocationRepositoryImpl locationRepository = new LocationRepositoryImpl();
+        LocationService locationService = new LocationService(locationRepository);
+        LocationController locationController = new LocationController(locationService);
+        UserInterface userInterface = new UserInterface(locationController);
+        userInterface.run();
     }
 }
